b4bb71afa16599a426ab7eda8cd1289feb505cdc
tosynthegeek <oluwatosinojedapo@gmail.com>
2024-12-11 11:13:23 +0100

    feat: added icp signers for canister tx

--- canisters/src/icp_backend/src/util.rs
+++ canisters/src/icp_backend/src/util.rs
@@ -1,4 +1,4 @@
-use alloy::hex;
+use alloy::{hex, signers::icp::IcpSigner};
 use hex::FromHexError;
 
 pub fn to_hex(data: &[u8]) -> String {
@@ -7,4 +7,19 @@ pub fn to_hex(data: &[u8]) -> String {
 
 pub fn from_hex(data: &str) -> Result<Vec<u8>, FromHexError> {
     hex::decode(&data[2..])
+}
+
+fn get_ecdsa_key_name() -> String {
+    #[allow(clippy::option_env_unwrap)]
+    let dfx_network = option_env!("DFX_NETWORK").unwrap();
+    match dfx_network {
+        "local" => "dfx_test_key".to_string(),
+        "ic" => "key_1".to_string(),
+        _ => panic!("Unsupported network."),
+    }
+}
+
+async fn create_icp_signer() -> IcpSigner {
+    let ecdsa_key_name = get_ecdsa_key_name();
+    IcpSigner::new(vec![], &ecdsa_key_name, None).await.unwrap()
 }
